39ef228a0b7c3b445eb4b8d2046dcd6b5c14d12c
Santa <s.yushmanov777@gmail.com>
2020-01-14 17:35:56 +0700

    create base api

---
+++ src/API.ts
@@ -0,0 +1,58 @@
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError, AxiosInstance } from 'axios';
+
+export interface IPrice {
+  success: boolean;
+  lowest_price?: string;
+  volume: string;
+  median_price?: string;
+}
+
+export interface IPriceHistory {
+  success: boolean;
+  price_prefix: string;
+  price_suffix: string;
+  prices: [string, number, string][];
+}
+
+export interface IPriceError {
+  success: false;
+}
+
+export interface IResponse<T> {
+  data: T;
+  status: number;
+}
+
+const handleResponse = <T>(response: AxiosResponse<T>): IResponse<T> => ({
+  data: response.data,
+  status: response.status
+});
+
+const handleError = (error: AxiosError): IResponse<IPriceError> => {
+  if (error.response) {
+    console.log(error.response.data);
+    console.log(error.response.status);
+    return { data: error.response.data || { success: false }, status: error.response.status };
+  }
+  console.log(error.message);
+  return { data: { success: false }, status: 500 };
+};
+
+const BASE_URL: string = 'https://steamcommunity.com/market';
+export const PRICE_OVERVIEW_URL = '/priceoverview';
+export const PRICE_HISTIRY_URL = '/pricehistory';
+
+type URL = typeof PRICE_OVERVIEW_URL | typeof PRICE_HISTIRY_URL;
+
+const config: AxiosRequestConfig = {
+  baseURL: BASE_URL,
+  withCredentials: true
+};
+
+const fetchAPI: AxiosInstance = axios.create(config);
+
+export const createReq = (appid: number, country: string, currency: number, market_hash_name: string) => (url: URL) =>
+  fetchAPI
+    .get(`${url}?appid=${appid}&country=${country}&currency=${currency}&market_hash_name=${market_hash_name}`)
+    .then(handleResponse)
+    .catch(handleError);
