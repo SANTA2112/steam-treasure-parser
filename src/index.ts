c56d181ac85560c15e413c240cce0b7d6f8dc5a0
Santa <s.yushmanov777@gmail.com>
2020-01-15 11:15:58 +0700

    test itemTypeInfo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-import { createReq, PRICE_HISTIRY_URL, PRICE_OVERVIEW_URL, IResponse, IPrice, IPriceHistory, IPriceError } from './API';
+import {
+  createReq,
+  PRICE_HISTIRY_URL,
+  PRICE_OVERVIEW_URL,
+  IResponse,
+  IPrice,
+  IPriceHistory,
+  IPriceError,
+  ITEM_TYPE_URL,
+  ItemType
+} from './API';
 import { init, getAveragePricePerYear } from './utils';
 
 (async () => {
@@ -9,8 +19,12 @@ import { init, getAveragePricePerYear } from './utils';
   const itemPriceHistrory: IResponse<IPriceHistory | IPriceError> = await createReq(
     PRICE_HISTIRY_URL(gameID, lng, currency, itemName)
   );
-  if (itemPrice.data.success && itemPriceHistrory.data.success) {
+  const itemTypeInfo: IResponse<ItemType | IPriceError> = await createReq(
+    ITEM_TYPE_URL(gameID, country, currency, itemName)
+  );
+  if (itemPrice.data.success && itemPriceHistrory.data.success && itemTypeInfo.data.success) {
     console.log(itemPrice.data.lowest_price);
     console.log(getAveragePricePerYear(itemPriceHistrory.data.prices));
+    console.log(itemTypeInfo.data.assets[gameID][2][Object.keys(itemTypeInfo.data.assets[gameID][2])[0]]);
   }
 })();
