bb9598bf3d1b6cd79c9afeb580c01124c819dab7
Santa <s.yushmanov777@gmail.com>
2020-01-18 13:42:49 +0700

    created modules (types, enums, interface)

---
+++ src/interfaces.ts
@@ -0,0 +1,79 @@
+import { Currency, Languages, CountryCode } from './enums';
+
+export interface IPrice {
+  success: boolean;
+  lowest_price?: string;
+  volume: string;
+  median_price?: string;
+}
+
+export interface IPriceHistory {
+  success: boolean;
+  price_prefix: string;
+  price_suffix: string;
+  prices: [string, number, string][];
+}
+export interface IItemTypeResponce {
+  success: boolean;
+  assets: {
+    [key: string]: {
+      '2': {
+        [key: string]: {
+          descriptions?: {
+            value: string;
+            color?: string;
+            type?: string;
+          }[];
+          name: string;
+          name_color: string;
+          type: string;
+          market_name: string;
+          market_hash_name: string;
+        };
+      };
+    };
+  };
+}
+
+export interface IPriceError {
+  success: false;
+  price_prefix?: string;
+  price_suffix?: string;
+  prices?: false;
+}
+
+export interface IResponse<T> {
+  data: T;
+  status: number;
+}
+
+export interface ICookie {
+  browserid: string;
+  timezoneOffset: string;
+  steamCountry: string;
+  recentapps: string;
+  sessionid: string;
+  app_impressions: string;
+  Steam_Language: string;
+}
+export interface IInit {
+  appid: string;
+  market_hash_name: string;
+  currency: Currency;
+  language: Languages;
+  country: CountryCode;
+}
+export interface ICountryInfo {
+  language: Languages;
+  currency: Currency;
+  countryCode: CountryCode;
+}
+export interface IItemProperties {
+  descriptions?: {
+    value: string;
+    color?: string;
+    type?: string;
+  }[];
+  name: string;
+  type: string;
+}
---
+++ src/types.ts
@@ -0,0 +1,21 @@
+export type Groups = {
+  [key: string]: string;
+};
+
+export type PricesPerYearArr = {
+  [key: string]: number[];
+};
+
+export type PricesPerYear = {
+  [key: string]: number;
+};
+
+export type PriceValues = [string, number, string][];
+
+export type ItemsType = 'case' | 'treasure' | 'container' | 'souvenir package';
+
+export type ItemDescPropValues = {
+  value: string;
+  color?: string;
+  type?: string;
+};
