85de7e48e68c4337f0e7e85eff142c8adc41cbf6
Santa <s.yushmanov777@gmail.com>
2020-01-19 13:13:18 +0700

    fixes and test funcs

--- src/index.ts
+++ src/index.ts
@@ -7,39 +7,43 @@ import {
   IItemProperties,
   ISubItem
 } from './interfaces';
-import { ItemDescPropValues, ItemsType } from './types';
+import { ItemDescPropValues, ItemsType, PricesPerYear } from './types';
 
 import { doReq } from './API';
-import { init, getAveragePricePerYear, findItemsInTreause, getItemType, getSubItemsSetParams, sleep } from './utils';
+import { init, getAveragePricePerYear, findItemsInTreause, getItemType, getSubItemsSetParams } from './utils';
 import { PRICE_HISTIRY_URL, PRICE_OVERVIEW_URL, ITEM_TYPE_URL } from './constants';
 
-(async () => {
+const main = async () => {
   const { appid, currency, market_hash_name, language, country } = init();
 
   const itemPrice: IResponse<IPrice | IPriceError> = await doReq(
-    PRICE_OVERVIEW_URL(appid, language, currency, market_hash_name)
+    PRICE_OVERVIEW_URL(appid, country, currency, market_hash_name)
   );
   const itemPriceHistrory: IResponse<IPriceHistory | IPriceError> = await doReq(
-    PRICE_HISTIRY_URL(appid, language, currency, market_hash_name)
+    PRICE_HISTIRY_URL(appid, country, currency, market_hash_name)
   );
   const itemTypeInfo: IResponse<IItemTypeResponce | IPriceError> = await doReq(
-    ITEM_TYPE_URL(appid, country, currency, market_hash_name)
+    ITEM_TYPE_URL(appid, language, currency, market_hash_name)
   );
 
   if (itemPrice.data.success && itemPriceHistrory.data.success && itemTypeInfo.data.success) {
-    console.log(itemPrice.data.lowest_price);
-    console.log(getAveragePricePerYear(itemPriceHistrory.data.prices));
+    const lowestPrice: string | undefined = itemPrice.data.lowest_price;
+    const averagePricePerYear: PricesPerYear = getAveragePricePerYear(itemPriceHistrory.data.prices);
     const itemInfo: IItemProperties =
       itemTypeInfo.data.assets[appid][2][Object.keys(itemTypeInfo.data.assets[appid][2])[0]];
     const itemType: ItemsType | void = getItemType(itemInfo);
-    console.log(itemType);
     if (itemType) {
+      let counter = 1;
       const items: ItemDescPropValues[] = findItemsInTreause(appid, itemType, itemInfo);
+      itemInfo.descriptions = items;
       const getSubItems: (item: ItemDescPropValues) => Promise<ISubItem[]> = getSubItemsSetParams(appid, itemType);
-      for (let item of items) {
-        const result: ISubItem[] = await getSubItems(item);
-        console.log(result);
+      for (let item of itemInfo.descriptions) {
+        item.subitems = await getSubItems(item);
+        console.log(`[${counter++}/${itemInfo.descriptions.length}] - ${item.value}`);
       }
+      console.log(JSON.stringify(itemInfo));
     }
   }
-})();
+};
+
+main();
