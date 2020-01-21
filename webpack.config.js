4bd41ddfea8057ce38d160150fe928bc880fe2cc
Santa <s.yushmanov777@gmail.com>
2020-01-21 17:21:09 +0700

    add css-loaders

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
         test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
