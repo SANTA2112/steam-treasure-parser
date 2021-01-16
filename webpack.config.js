93dfa298bac19b4764d81afc0740ba5900aec5fb
Santa <s.yushmanov777@gmail.com>
2021-01-16 11:24:44 +0700

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,30 +5,29 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.ts'
+    index: './src/index.ts',
   },
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'script.js',
     chunkFilename: 'script.js',
-    publicPath: '/'
+    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         use: 'ts-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
-  devtool: process.env.MODE === 'production' ? 'none' : 'inline-sourse-map',
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
   },
-  plugins: [new webpack.ProgressPlugin(), new CopyPlugin([{ from: 'public', to: '' }])]
+  plugins: [new webpack.ProgressPlugin(), new CopyPlugin({ patterns: [{ from: 'public', to: '' }] })],
 };
