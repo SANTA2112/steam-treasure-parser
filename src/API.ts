9385ce91ed765d5b1118ff98b139bf23d3160dfd
Santa <s.yushmanov777@gmail.com>
2021-03-18 13:56:28 +0700

    pretty

--- src/API.ts
+++ src/API.ts
@@ -7,7 +7,7 @@ import { BASE_URL } from './constants';
 
 const config: AxiosRequestConfig = {
   baseURL: BASE_URL,
-  withCredentials: true
+  withCredentials: true,
 };
 
 const handleResponse = <T>(response: AxiosResponse<T>): AxiosResponse<T> => response;
@@ -25,8 +25,4 @@ const handleError = (error: AxiosError): IResponse<IPriceError> => {
 
 const fetchAPI: AxiosInstance = axios.create(config);
 
-export const doReq = (url: string) =>
-  fetchAPI
-    .get(url)
-    .then(handleResponse)
-    .catch(handleError);
+export const doReq = (url: string) => fetchAPI.get(url).then(handleResponse).catch(handleError);
