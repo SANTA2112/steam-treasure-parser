30a23ab08b672e4c9c6a697645ff07bd679dc352
Santa <s.yushmanov777@gmail.com>
2021-08-22 03:29:12 +0700

    full refactor sales quantity parser

--- src/utils.ts
+++ src/utils.ts
@@ -28,11 +28,31 @@ const getLastWeek = (today: Date) => new Date(today.getFullYear(), today.getMont
 const getLastMonth = (today: Date) => new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
 const getLastYear = (today: Date) => new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
 
-const getPriceIndexByDateSetParams = (prices: PriceValues) => (day: number, month: string, year: number) =>
-  prices.findIndex(([priceDate]) => {
-    const [monthPrice, dayPrice, yearPrice] = priceDate.split(' ');
-    return +dayPrice === day && monthPrice === month && +yearPrice === year;
-  });
+const getDatesRange = (date1: Date, date2: Date) => {
+  const dates = [];
+  const range = Math.floor((date1.getTime() - date2.getTime()) / 1000 / 60 / 60 / 24);
+  let currentDate = date1;
+
+  for (let i = 0; i < range; i++) {
+    dates.push(getLastDay(currentDate));
+    currentDate = getLastDay(currentDate);
+  }
+
+  return dates;
+};
+
+const getPriceIndexByDateSetParams = (prices: PriceValues) => (dates: Date[]) => {
+  for (const date of dates) {
+    const index = prices.findIndex(([priceDate]) => {
+      const [monthPrice, dayPrice, yearPrice] = priceDate.split(' ');
+      return (
+        +dayPrice === date.getDate() && monthPrice === months[date.getMonth()] && +yearPrice === date.getFullYear()
+      );
+    });
+    if (index !== -1) return index;
+  }
+  return -1;
+};
 
 export const sleep = (ms: number): Promise<void> => new Promise((r) => setTimeout(r, ms));
 
@@ -324,19 +344,14 @@ export const getQuantityOfSales = (prices: PriceValues): TQuantityOfSales => {
 
   const getPriceIndexByDate = getPriceIndexByDateSetParams(prices);
 
-  const findIndexByDate = (date: Date, depth = 10): number => {
-    const index = getPriceIndexByDate(date.getDate(), months[date.getMonth()], date.getFullYear());
-    return depth === 0 || index !== -1 ? index : findIndexByDate(getLastDay(date), depth - 1);
-  };
-
   const [salesPerDay, salesPerWeek, salesPerMonth, salesPerYear] = [
     getLastDay(date),
     getLastWeek(date),
     getLastMonth(date),
     getLastYear(date),
   ]
-    .map((neededDate) => findIndexByDate(neededDate))
-    .map((index) => prices.slice(index).reduce((a, [, , quantity]) => +quantity + a, 0));
+    .map((neededDate) => getPriceIndexByDate(getDatesRange(date, neededDate).reverse()))
+    .map((index) => (index !== -1 ? prices.slice(index).reduce((a, [, , quantity]) => +quantity + a, 0) : 0));
 
   return {
     day: salesPerDay.toLocaleString(),
