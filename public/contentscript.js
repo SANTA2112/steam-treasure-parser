b705b3f699b07a0ab4c78bc0fe1b19bb6a9cb2e1
Santa <s.yushmanov777@gmail.com>
2022-04-29 18:19:33 +0700

    small refactor content script

--- public/contentscript.js
+++ public/contentscript.js
@@ -1,6 +1,6 @@
-var s = document.createElement('script');
-s.src = chrome.extension.getURL('script.js');
-(document.head || document.documentElement).appendChild(s);
-s.onload = function () {
+const s = document.createElement('script');
+s.src = chrome.runtime.getURL('script.js');
+(document.head || document.body).appendChild(s);
+s.onload = () => {
   s.parentNode.removeChild(s);
 };
