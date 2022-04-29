4834c6f3ccd32a7e2a0aa9a37581b0bd392b0cbe
Santa <s.yushmanov777@gmail.com>
2022-04-29 14:18:26 +0700

    add chaining op

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,7 +2,7 @@ export const addSelectListener = (): void => {
   document.addEventListener<'click'>('click', (event) => {
     if (event.target) {
       const { parentElement } = event.target as unknown as HTMLElement;
-      if (parentElement && parentElement.classList.contains('select-stp')) parentElement.classList.toggle('active');
+      if (parentElement?.classList.contains('select-stp')) parentElement.classList.toggle('active');
     }
   });
 };
