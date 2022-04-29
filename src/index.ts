01e251b81dbfab1076c8bb28518762febd5c21ce
Santa <s.yushmanov777@gmail.com>
2022-04-29 18:22:38 +0700

    refactor main script

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,9 @@ import toastr from 'toastr';
 import 'toastr/build/toastr.min.css';
 import './_assets/css/style.css';
 
-import {
-  IPriceError,
-  IItemTypeResponce,
-  IResponse,
-  IItemProperties,
-  IItemPropertyDescription,
-  IItemInfo,
-  priceByQuarters,
-} from './interfaces';
+import { IItemPropertyDescription } from './interfaces';
 
-import { TQuantityOfSales } from './types';
-
-import { doReq } from './api';
-
-import { ITEM_INFO_URL, ITEM_TYPE_URL, toastrOptions } from './constants';
+import { toastrOptions } from './constants';
 import { getQuantityOfSales } from './utils/dates';
 import { init } from './utils/init';
 import { renderQuantityOfSales, findItemsInTreause, giveItemsPriceSetParams } from './utils/items';
@@ -26,47 +14,33 @@ import { getAveragePricePerQuarters, renderAveragePricePerQuarters } from './uti
 toastr.options = toastrOptions;
 
 const main = async () => {
-  const { appid, currency, market_hash_name, language, country, prices, item_nameid } = init();
-
-  const itemPrice: IResponse<IItemInfo | IPriceError> = await doReq(ITEM_INFO_URL(appid, market_hash_name, 1));
-
-  let pricePrefix = '';
-  let priceSuffix = '';
-  const itemPriceHistroryScript: HTMLScriptElement | undefined = [...document.querySelectorAll('script')].find(
-    (script) => script.textContent?.includes('var line1='),
-  );
-
-  if (itemPriceHistroryScript) {
-    pricePrefix = JSON.parse(itemPriceHistroryScript.textContent?.match(/var strFormatPrefix = (.+?);/)?.[1]!);
-    priceSuffix = JSON.parse(itemPriceHistroryScript.textContent?.match(/var strFormatSuffix = (.+?);/)?.[1]!);
-  }
-
-  const itemTypeInfo: IResponse<IItemTypeResponce | IPriceError> = await doReq(
-    ITEM_TYPE_URL(appid, language, currency, market_hash_name),
-  );
+  const {
+    appid,
+    currency,
+    language,
+    country,
+    prices,
+    item_nameid,
+    item_info,
+    item_price,
+    market_hash_name,
+    price_suffix,
+  } = init();
 
   const itemNode = document.querySelector<HTMLDivElement>('#largeiteminfo_item_name');
 
-  if (itemPrice.data.success && itemTypeInfo.data.success && itemNode) {
-    const averagePricePerQuarters: priceByQuarters = getAveragePricePerQuarters(prices);
-    const quantityOfSales: TQuantityOfSales = getQuantityOfSales(prices);
-    const itemInfo: IItemProperties =
-      itemTypeInfo.data.assets[appid][Object.keys(itemTypeInfo.data.assets[appid])[0]][
-        Object.keys(itemTypeInfo.data.assets[appid][Object.keys(itemTypeInfo.data.assets[appid])[0]])[0]
-      ];
-
-    itemNode.insertAdjacentHTML('beforeend', `<div>Price: ${itemPrice.data.results[0].sell_price_text}</div>`);
-    renderAveragePricePerQuarters(averagePricePerQuarters, itemNode, pricePrefix, priceSuffix);
+  if (itemNode) {
+    const averagePricePerQuarters = getAveragePricePerQuarters(prices);
+    const quantityOfSales = getQuantityOfSales(prices);
+    itemNode.insertAdjacentHTML('beforeend', `<div>Price: ${Number(item_price) / 100} ${price_suffix}</div>`);
+    renderAveragePricePerQuarters(averagePricePerQuarters, itemNode, price_suffix);
     renderQuantityOfSales(quantityOfSales, itemNode);
 
-    itemInfo.descriptions = findItemsInTreause(appid, itemInfo);
+    item_info.descriptions = findItemsInTreause(appid, item_info);
 
-    if (itemInfo.descriptions.length !== 0) {
-      const giveItemsPrice: (item: IItemPropertyDescription) => Promise<void> = giveItemsPriceSetParams(
-        appid,
-        pricePrefix,
-      );
-      await parallel<IItemPropertyDescription, void>(itemInfo.descriptions, giveItemsPrice, {
+    if (item_info.descriptions.length !== 0) {
+      const giveItemsPrice = giveItemsPriceSetParams(appid);
+      await parallel<IItemPropertyDescription, void>(item_info.descriptions, giveItemsPrice, {
         streams: 2,
         timeout: 100,
       });
