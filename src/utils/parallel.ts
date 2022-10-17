e8815339c13bf0f589bdb12a59ffb55177b5c4a0
santa <s.yushmanov777@gmail.com>
2022-10-17 16:10:51 +0700

    refactor parallel

--- src/utils/parallel.ts
+++ src/utils/parallel.ts
@@ -1,59 +1,99 @@
 /* eslint-disable consistent-return */
-export interface IFetcher<T, R> {
-  (arg: T): Promise<R | R[]>;
+/* eslint-disable no-promise-executor-return */
+export interface Queue<T> {
+  retryCount: number;
+  value: T;
 }
 
-export interface IFetchError<T> {
-  message: string;
-  src: T;
+export type Handler<T, R> = (value: T) => Promise<R>;
+
+export interface ErrorHandlerArg<T> {
+  error: Error;
+  item: T;
+  retryCount: number;
 }
 
-export interface IOptions {
-  /** Колличество одновременных запросов */
-  streams?: number;
-  /** Время задержки между запросами в ms */
+export type ErrorHandler<T> = (args: ErrorHandlerArg<T>) => void | true;
+
+export interface SuccessLogArgs<T> {
+  currentValue: T;
+  counter: number;
+  length: number;
+  inProgress: number;
+}
+
+export type SuccessLogHandler<T> = (args: SuccessLogArgs<T>) => void;
+
+export interface Options<T, R> {
+  concurrency?: number;
   timeout?: number;
+  needResults?: boolean;
+  handler: Handler<T, R>;
+  errorHandler: ErrorHandler<T>;
+  successLog?: SuccessLogHandler<T>;
 }
 
-export interface IDone<T, R> {
-  results: R[];
-  errors: IFetchError<T>[];
-}
+export const waitFor = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-export const parallel = async <T, R>(
-  /** Массив со входными данными */
-  arr: T[],
-  /** Функция которая принимает входной элемент, производит с ним действия и возвращает промис */
-  fetcher: IFetcher<T, R>,
-  { streams = 10, timeout = 1000 }: IOptions = {},
-): Promise<IDone<T, R>> => {
-  const source: T[] = [...arr];
-  const results: R[] = [];
-  const errors: IFetchError<T>[] = [];
-  const tasks: Promise<any>[] = [];
-
-  const wait = (ms: number): Promise<void> =>
-    new Promise((r) => {
-      setTimeout(r, ms);
-    });
-
-  const run = async (): Promise<any> => {
-    if (source.length === 0) return;
-    const current = source.shift() as T;
-
-    return fetcher(current)
-      .then((result) => (Array.isArray(result) ? results.push(...result) : results.push(result)))
-      .catch((err: Error) => errors.push({ src: current, message: err.message }))
-      .then(() => wait(timeout))
-      .then(run);
+export const parallel = async <T, R>(data: T[], options: Options<T, R>) => {
+  const {
+    concurrency = 5,
+    timeout = 1000,
+    needResults = true,
+    handler,
+    errorHandler,
+    successLog = () => {},
+  } = {
+    ...options,
   };
 
-  for (let i = 0; i < streams; i++) tasks.push(run());
+  const queue: Queue<T>[] = data.map((value) => ({ value, retryCount: 0 }));
+  const results: R[] = [];
+  const errors = new Map<T, Error[]>();
+  const tasks: Set<Promise<void>> = new Set();
+  let counter = 0;
+
+  const addError = (key: T, error: Error) => {
+    errors.set(key, [...(errors.get(key) || []), error as Error]);
+  };
+
+  const runThread = async (item: Queue<T>): Promise<void> => {
+    try {
+      const result = await handler(item.value);
+      if (needResults) results.push(result);
+      successLog({
+        currentValue: item.value,
+        counter: (counter += 1),
+        inProgress: tasks.size,
+        length: queue.length,
+      });
+      errors.delete(item.value);
+    } catch (e) {
+      addError(item.value, e as Error);
+
+      const needToRetry = errorHandler({
+        error: e as Error,
+        item: item.value,
+        retryCount: item.retryCount,
+      });
+
+      if (needToRetry) {
+        item.retryCount += 1;
+        return runThread(item);
+      }
+    } finally {
+      await waitFor(timeout);
+    }
+  };
+
+  for (const item of queue) {
+    const thread = runThread(item);
+    tasks.add(thread);
+    thread.then(() => tasks.delete(thread));
+    if (tasks.size >= Math.min(concurrency, queue.length)) await Promise.race(tasks);
+  }
 
   await Promise.all(tasks);
 
-  return {
-    results,
-    errors,
-  };
+  return [results, errors] as const;
 };
