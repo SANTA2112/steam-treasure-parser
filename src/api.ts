82ca33798bd18c70bde626f467de207e2ce7845c
santa <s.yushmanov777@gmail.com>
2023-02-03 21:27:04 +0700

    refactor api

--- src/api.ts
+++ src/api.ts
@@ -1,28 +1,28 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import ky, { Options } from 'ky';
 import toastr from 'toastr';
 
 import { IItemInfo, SteamPrice } from './interfaces';
 
-const config: AxiosRequestConfig = {
-  baseURL: 'https://steamcommunity.com/market',
-  withCredentials: true,
+const config: Options = {
+  prefixUrl: 'https://steamcommunity.com/market/',
 };
 
-const instance = axios.create(config);
-
-instance.interceptors.response.use(
-  (response) => response.data,
-  (error) => (toastr.error(error.message), error),
-);
+const instance = ky
+  .create(config)
+  .extend({ hooks: { beforeError: [(error) => (toastr.error(error.message), error)] } });
 
 export const fetchItemInfo = (appid: string, market_hash_name: string, resultsCount: number = 1) => {
-  return instance.get<void, IItemInfo>(
-    `/search/render/?search_descriptions=1&count=${resultsCount}&appid=${appid}&norender=1&query=${market_hash_name}`,
-  );
+  return instance
+    .get(
+      `search/render/?search_descriptions=1&count=${resultsCount}&appid=${appid}&norender=1&query=${market_hash_name}`,
+    )
+    .json<IItemInfo>();
 };
 
 export const fetchItemPrice = (country: string, language: string, currency: number, item_nameid: string) => {
-  return instance.get<void, SteamPrice>(
-    `/itemordershistogram?country=${country}&language=${language}&currency=${currency}&item_nameid=${item_nameid}&two_factor=0`,
-  );
+  return instance
+    .get(
+      `itemordershistogram?country=${country}&language=${language}&currency=${currency}&item_nameid=${item_nameid}&two_factor=0`,
+    )
+    .json<SteamPrice>();
 };
