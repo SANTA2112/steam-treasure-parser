37b675e7bc60bf0bc5b3a2dfd4ba5b2a1f0bfc76
santa <s.yushmanov777@gmail.com>
2024-08-27 02:12:25 +0700

    removed webpack, added vite

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { resolve } from 'node:path';
+
+import { defineConfig } from 'vite';
+
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+
+export default defineConfig({
+  root: resolve(process.cwd()),
+  build: {
+    outDir: resolve(process.cwd(), 'build'),
+    rollupOptions: {
+      input: resolve(process.cwd(), 'src/index.ts'),
+      output: {
+        dir: resolve(process.cwd(), 'build'),
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
+  plugins: [cssInjectedByJsPlugin()],
+});
