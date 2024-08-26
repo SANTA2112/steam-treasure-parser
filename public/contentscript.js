e46e6b2bede22038fd95b3b5f8eeb403ccbee562
santa <s.yushmanov777@gmail.com>
2024-08-27 02:12:41 +0700

    rename main script

--- public/contentscript.js
+++ public/contentscript.js
@@ -1,5 +1,5 @@
 const s = document.createElement('script');
-s.src = chrome.runtime.getURL('script.js');
+s.src = chrome.runtime.getURL('index.js');
 (document.head || document.body).appendChild(s);
 s.onload = () => {
   s.parentNode.removeChild(s);
