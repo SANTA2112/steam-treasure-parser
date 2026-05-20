d380d60b17c0eece868d1da05b4223fea549c375
santa <s.yushmanov777@gmail.com>
2026-05-20 22:35:46 +0700

    update vite config

--- vite.config.overrideFetch.ts
+++ vite.config.overrideFetch.ts
@@ -7,16 +7,19 @@ import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 export default defineConfig({
   root: resolve(process.cwd()),
   build: {
+    emptyOutDir: false,
     sourcemap: false,
     minify: 'terser',
+    // minify: false,
     outDir: resolve(process.cwd(), 'build'),
     rollupOptions: {
-      input: resolve(process.cwd(), 'src/index.ts'),
+      input: resolve(process.cwd(), 'src/overrideFetch.ts'),
       output: {
         dir: resolve(process.cwd(), 'build'),
         entryFileNames: '[name].js',
         chunkFileNames: '[name].js',
         assetFileNames: '[name].[ext]',
+        format: 'iife',
       },
     },
   },
